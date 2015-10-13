1e1c7cdc1dcd00d0aaa692a2c995365e9d2d16fe
mesnardo <mesnardo@gwu.edu>
2015-10-13 12:11:38 -0400

    Add citations number and pull brief note

--- libraries/pyBiblio.py
+++ libraries/pyBiblio.py
@@ -2,7 +2,7 @@
 
 # file: pyBiblio.py
 # author: Olivier Mesnard (mesnardo@gwu.edu)
-# brief: Library to manage references.
+# brief: Library manager.
 
 
 import os
@@ -89,6 +89,17 @@ class ReferenceManager(object):
       for note in self.notes.itervalues():
         if note.search(connection=connection):
           note.print_info()
+
+  def pull_brief_note(self, key=None):
+    """Pulls the brief notes about a paper given its key.
+
+    Parameters
+    ----------
+    key: string
+      Key of the paper; default: None.
+    """
+    if key in self.notes.keys():
+      self.notes[key].pull_brief()
         
 
 class PaperNote(object):
@@ -107,6 +118,7 @@ class PaperNote(object):
     self.authors = []
     self.year = None
     self.keywords = []
+    self.citations = None
     self.connections = []
     self.parse_info()
 
@@ -120,12 +132,13 @@ class PaperNote(object):
     print('- title: {}'.format(self.title))
     print('- authors: {}'.format(', '.join(self.authors)))
     print('- year: {}'.format(self.year))
+    print('- citations: {}, {}'.format(self.citations['count'], self.citations['info']))
     print('- keywords: {}'.format(', '.join(self.keywords)))
     if self.connections:
       print('- list of connections:')
       for key, reason in self.connections.iteritems():
-        print('\t- key: {}'.format(key))
-        print('\t- reason: {}'.format(reason))
+        print('  - key: {}'.format(key))
+        print('  - reason: {}'.format(reason))
     print('-'*80)
 
   def parse_info(self):
@@ -148,6 +161,7 @@ class PaperNote(object):
       self.authors = self.set('authors', lines)
       self.year = self.set('year', lines)
       self.keywords = self.set('keywords', lines)
+      self.citations = self.set('citations', lines)
       self.connections = self.set('connections', lines)
 
   def set(self, look_for, lines):
@@ -166,13 +180,18 @@ class PaperNote(object):
       if re.search(look_for, line):
         if look_for == 'key':
           return line.split(':')[-1].strip()
-        if look_for == 'title':
+        elif look_for == 'title':
           return line.split(':')[-1].strip()
-        if look_for == 'authors':
+        elif look_for == 'authors':
           return re.findall(r"[\w']+", line.split(':')[-1])
-        if look_for == 'year':
+        elif look_for == 'year':
           return int(line.split(':')[-1].strip())
-        if look_for == 'keywords':
+        elif look_for == 'citations':
+          line_split = line.strip().split(':')[-1].split(',')
+          count = int(line_split[0])
+          info = line_split[1]
+          return {'count': count, 'info': info} 
+        elif look_for == 'keywords':
           return [keyword.strip() 
                   for keyword in re.findall(r"[^,;]+", line.split(':')[-1])]
 
@@ -192,8 +211,11 @@ class PaperNote(object):
     connections = {}
     for line in lines:
       if re.search('connection', line):
-        data = line.strip().split(':')[-1].strip().split(',')
-        connections[data[0].strip()] = data[1].strip()
+        connection = line.strip().split(':')[1]
+        if connection:
+          line_split = connection.strip().split('[')
+          key, reason = line_split[0].strip(), line_split[1][:-1]
+          connections[key] = reason
     return connections
 
   def search(self, key=None, keyword=None, author=None, year=None, connection=None):
@@ -219,13 +241,28 @@ class PaperNote(object):
     if key:
       return key == self.key
     elif keyword:
-      return keyword in self.keywords
+      return keyword.lower() in [k.lower() for k in self.keywords]
     elif author:
-      return author in self.authors
+      return author.lower() in [a.lower() for a in self.authors]
     elif year:
       return year == self.year
     elif connection:
-      return connection in self.connections.iterkeys()
+      return connection in self.connections.keys()
+
+  def pull_brief(self):
+    """Pulls the brief paragraph about the paper from the note."""
+    with open(self.path, 'r') as infile:
+      lines = infile.readlines()
+    index_start, index_end = None, None
+    for index, line in enumerate(lines):
+      if re.search('## In brief', line) and not index_end:
+        index_start = index + 1
+      if re.search('## In details', line) and index_start:
+        index_end = index - 2
+    self.print_info()
+    lines = '\n'.join(lines[index_start: index_end])
+    print('\nIn brief:\n')
+    print('\n'.join(line.strip() for line in re.findall(r'.{1,80}(?:\s+|$)', lines)))
 
 
 def parse_command_line():
@@ -252,6 +289,9 @@ def parse_command_line():
   parser.add_argument('--connection', dest='connection',
                       type=str,
                       help='key of the note to print connections with')
+  parser.add_argument('--pull', dest='pull_brief', 
+                      type=str, default=None,
+                      help='key of the paper from which to pull the brief note')
   return parser.parse_args()
         
 
@@ -264,6 +304,7 @@ def main():
   manager.search_notes(keys=parameters.keys, keywords=parameters.keywords,
                        authors=parameters.authors, years=parameters.years,
                        connection=parameters.connection)
+  manager.pull_brief_note(key=parameters.pull_brief)
 
 
 if __name__ == '__main__':
