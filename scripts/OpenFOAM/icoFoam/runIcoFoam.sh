4cd4a693ad4767cb01e1eaca9fe23ef1dd2e879f
mesnardo <mesnardo@gwu.edu>
2015-10-21 10:25:31 -0400

    Create sub-dir log.run

--- scripts/OpenFOAM/icoFoam/runIcoFoam.sh
+++ scripts/OpenFOAM/icoFoam/runIcoFoam.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+# file: runIcoFoam.sh
+# author: Olivier Mesnard (mesnardo@gwu.edu)
+# brief: Runs IcoFOAM in serial.
+
+
+# remove previous run
+rm -rf log.run && mkdir log.run
 
 # source tool run functions
 . $WM_PROJECT_DIR/bin/tools/RunFunctions
 
 # run icofoam
 runApplication icoFoam
+mv log.icofoam log.run
--- scripts/OpenFOAM/icoFoam/runIcoFoam_colonialOne.sh
+++ scripts/OpenFOAM/icoFoam/runIcoFoam_colonialOne.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# file: runIcoFoam_colonialOne.sh
+# author: Olivier Mesnard (mesnardo@gwu.edu)
+# brief: Runs IcoFOAM on multi-nodes on Colonial One.
+
 #SBATCH --job-name="OFfs25n32"
 #SBATCH --output=log%j.out
 #SBATCH --error=log%j.err
@@ -9,11 +13,16 @@
 
 module load openfoam/gcc/2.3.0
 
+# remove previous run
+rm -rf log.run && mkdir log.run
+
 # source tool run functions
 . $WM_PROJECT_DIR/bin/tools/RunFunctions
 
 # decompose domain for parrallel run
 runApplication decomposePar
+mv log.decomposePar log.run
 
 # run icofoam
 time runParallel icoFoam -parallel
+mv log.icofoam log.run
