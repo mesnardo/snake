d35c4b551a285db8aa49bdff2d1c642f36cbb11f
mesnardo <mesnardo@gwu.edu>
2015-10-26 16:57:51 -0400

    Extrude before snapping

--- scripts/OpenFOAM/snappyHexMesh/runSnappyHexMeshColonialOne.sh
+++ scripts/OpenFOAM/snappyHexMesh/runSnappyHexMeshColonialOne.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# file: generateSnappyHexMesh_serial_colonialOne.sh
+# file: runSnappyHexMeshColonialOne.sh
 # author: Olivier Mesnard (mesnardo@gwu.edu)
 # brief: Generates a mesh with snappyHexMesh on 1 processor on Colonial One.
 
-#SBATCH --job-name="sHMSerial"
+#SBATCH --job-name="sHM"
 #SBATCH --output=log%j.out
 #SBATCH --error=log%j.err
 #SBATCH --partition=short
@@ -16,22 +16,35 @@ module load openfoam/gcc/2.3.0
 # source tool run functions
 . $WM_PROJECT_DIR/bin/tools/RunFunctions
 
-# clean previous mesh
+rm -rf log.mesh && mkdir log.mesh
 find constant/polyMesh -type f \! -name 'blockMeshDict' -delete
-rm -rf log.mesh
-mkdir log.mesh
 
 # create base mesh
 runApplication blockMesh
 mv log.blockMesh log.mesh
 
 # create castellated mesh
+cp system/snappyHexMeshDict_resources/snappyHexMeshDict.castellated system/snappyHexMeshDict
 runApplication snappyHexMesh -overwrite
-mv log.snappyHexMesh log.mesh
+mv log.snappyHexMesh log.mesh/log.snappyHexMesh.castellated
+
+# flatten front and back planes of a 2d Cartesian mesh
+runApplication flattenMesh
+mv log.flattenMesh log.mesh
 
 # extrude mesh in third direction
 runApplication extrudeMesh
 mv log.extrudeMesh log.mesh
+
+# clean constant/polyMesh directory
+cd constant/polyMesh
+rm -rf cellLevel level0Edge pointLevel refinementHistory surfaceIndex
+cd ../..
+
+# snap castellated mesh on surface
+cp system/snappyHexMeshDict_resources/snappyHexMeshDict.snapped system/snappyHexMeshDict
+runApplication snappyHexMesh -overwrite
+mv log.snappyHexMesh log.mesh/log.snappyHexMesh.snapped
 
 # create patches
 runApplication createPatch -overwrite
