256c0af447d850bb9b992dd33066f709fb2ed77a
mesnardo <mesnardo@gwu.edu>
2015-11-02 18:11:51 -0500

    Backward compatibility for non-binary read

--- scripts/cuIBM/ioCuIBM.py
+++ scripts/cuIBM/ioCuIBM.py
@@ -5,10 +5,14 @@
 # description: Collection of IO functions for cuIBM.
 
 
+import os
 import sys
 import struct
 
 import numpy
+from matplotlib import pyplot, cm
+# load default style of matplotlib figures
+pyplot.style.use('{}/styles/mesnardo.mplstyle'.format(os.environ['SCRIPTS']))
 
 
 class Field(object):
@@ -64,6 +68,7 @@ def read_grid(directory, binary=False):
   x, y: Numpy 1d arrays of float
     Coordinates along a grid-line in each direction.
   """
+  print('[info] reading grid file ... '),
   grid_file = '{}/grid'.format(directory)
   if binary:
     with open(grid_file, 'rb') as infile:
@@ -82,6 +87,7 @@ def read_grid(directory, binary=False):
       # y-direction
       ny = int(data[0])
       y = data[1:]
+  print('done')
   return x, y
 
 
@@ -104,12 +110,13 @@ def read_velocity(directory, time_step, coords, binary=False):
   u, v: Field objects
     Information about the u- and v-velocities (nodes and values).
   """
+  print('[time-step {}] reading velocity field from file ... '.format(time_step)),
   # get info about the grid
   nx, ny = coords[0].size-1, coords[1].size-1
   x, y = coords
   dx, dy = x[1:]-x[:-1], y[1:]-y[:-1]
   # read fluxes from file
-  flux_file = '{}/{:0>7}'.format(directory, time_step)
+  flux_file = '{}/{:0>7}/q'.format(directory, time_step)
   if binary:
     with open(flux_file, 'rb') as infile:
       nq = struct.unpack('i', infile.read(4))[0]
@@ -129,8 +136,9 @@ def read_velocity(directory, time_step, coords, binary=False):
   for j in xrange(ny-1):
     for i in xrange(nx):
       v[j*nx+i] = q[offset+j*nx+i] / dx[i]
-  return (Field(x[1:-1], 0.5*(y[:-1]+y[1:]), u, label='u-velocity'), 
-          Field(0.5*(x[:-1]+x[1:]), y[1:-1], v, label='v-velocity'))
+  print('done')
+  return (Field(x[1:-1], 0.5*(y[:-1]+y[1:]), u.reshape(ny, nx-1), label='u-velocity'), 
+          Field(0.5*(x[:-1]+x[1:]), y[1:-1], v.reshape(ny-1, nx), label='v-velocity'))
 
 
 def read_pressure(directory, time_step, coords, binary=False):
@@ -152,10 +160,12 @@ def read_pressure(directory, time_step, coords, binary=False):
   p: Field object
     Information about the pressure field (nodes and values).
   """
+  print('[time-step {}] reading pressure field from file ... '.format(time_step)),
   # get info about mesh-grid
   nx, ny = coords[0].size-1, coords[1].size-1
+  x, y = coords
   # read pressure from file
-  lambda_file = '{}/{:0>7}'.format(directory, time_step)
+  lambda_file = '{}/{:0>7}/lambda'.format(directory, time_step)
   if binary:
     with open(lambda_file, 'rb') as infile:
       nlambda = struct.unpack('i', infile.read(4))[0]
@@ -164,7 +174,8 @@ def read_pressure(directory, time_step, coords, binary=False):
     with open(lambda_file, 'r') as infile:
       nlambda = int(infile.readline())
       p = numpy.loadtxt(infile, dtype=float)[:nx*ny]
-  return Field(0.5*(x[:-1]+x[1:]), 0.5*(y[:-1]+y[1:]), p, label='pressure')
+  print('done')
+  return Field(0.5*(x[:-1]+x[1:]), 0.5*(y[:-1]+y[1:]), p.reshape(nx, ny), label='pressure')
 
 
 def read_mask(directory, nx, ny):
@@ -182,10 +193,88 @@ def read_mask(directory, nx, ny):
   mask: Numpy 1d array of integers
     Masks in the x- and y-directions.
   """
+  print('[info] reading the mask from file ... '),
   mask_file = '{}/mask.txt'.format(directory)
   mask = numpy.loadtxt(mask_file, dtype=int)
   offset = (nx-1) * ny
+  print('done')
   return mask[:offset], mask[offset:]
+
+
+def plot_contour(field, field_range, image_path, 
+                 view=[float('-inf'), float('-inf'), float('inf'), float('inf')],
+                 size=[8.0, 8.0], dpi=100): 
+  """Plots and saves the field.
+
+  Parameters
+  ----------
+  field: ioCuIBM.Field instance
+    Nodes and values of the field.
+  field_range: list(float)
+    Min, max and number of countours to plot.
+  image_path: str
+    Path of the image to save.
+  view: list(float)
+    Bottom-left and top-right coordinates of the rectangular view to plot;
+    default: the whole domain.
+  size: list(float)
+    Size (width and height) of the figure to save (in inches); default: [8, 8].
+  dpi: int
+    Dots per inch (resolution); default: 100
+  """
+  print('[info] plotting the {} contour ... '.format(field.label)),
+  fig, ax = pyplot.subplots(figsize=(size[0], size[1]), dpi=dpi)
+  pyplot.xlabel('$x$')
+  pyplot.ylabel('$y$')
+  if field_range:
+    levels = numpy.linspace(field_range[0], field_range[1], field_range[2])
+    colorbar_ticks = numpy.linspace(field_range[0], field_range[1], 5)
+  else:
+    levels = numpy.linspace(field.values.min(), field.values.max(), 101)
+    colorbar_ticks = numpy.linspace(field.values.min(), field.values.max(), 5)
+  X, Y = numpy.meshgrid(field.x, field.y)
+  color_map = {'pressure': cm.jet, 'vorticity': cm.RdBu_r,
+               'u-velocity': cm.RdBu_r, 'v-velocity': cm.RdBu_r}
+  cont = ax.contourf(X, Y, field.values, 
+                     levels=levels, extend='both', 
+                     cmap=color_map[field.label])
+  cont_bar = fig.colorbar(cont, label=field.label, 
+                          orientation='horizontal', format='%.02f', 
+                          ticks=colorbar_ticks)
+  x_start, x_end = max(view[0], field.x.min()), min(view[2], field.x.max())
+  y_start, y_end = max(view[1], field.y.min()), min(view[3], field.y.max())
+  ax.axis([x_start, x_end, y_start, y_end])
+  ax.set_aspect('equal')
+  pyplot.savefig(image_path, dpi=dpi)
+  pyplot.close()
+  print('done')
+
+
+def compute_vorticity(u, v):
+  """Computes the vorticity field for a two-dimensional simulation.
+
+  Parameters
+  ----------
+  u, v: ioCuIBM.Field objects
+    u-velocity and v-velocity fields.
+
+  Returns
+  -------
+  vorticity: ioCuIBM.Field object
+    The vorticity field.
+  """
+  print('[info] computing the vorticity field ... '),
+  mask_x = numpy.where(numpy.logical_and(u.x > v.x[0], u.x < v.x[-1]))[0]
+  mask_y = numpy.where(numpy.logical_and(v.y > u.y[0], v.y < u.y[-1]))[0]
+  # vorticity nodes at cell vertices intersection
+  xw, yw = 0.5*(v.x[:-1]+v.x[1:]), 0.5*(u.y[:-1]+u.y[1:])
+  # compute vorticity
+  w = ( (v.values[mask_y, 1:] - v.values[mask_y, :-1])
+        / numpy.outer(numpy.ones(yw.size), v.x[1:]-v.x[:-1])
+      - (u.values[1:, mask_x] - u.values[:-1, mask_x])
+        / numpy.outer(u.y[1:]-u.y[:-1], numpy.ones(xw.size)) )
+  print('done')
+  return Field(x=xw, y=yw, values=w, label='vorticity')
 
 
 if __name__ == '__main__':
--- scripts/cuIBM/plotFields2d.py
+++ scripts/cuIBM/plotFields2d.py
@@ -9,15 +9,13 @@ import os
 import sys
 import argparse
 
-import numpy
-from matplotlib import pyplot, cm
-
 sys.path.append('{}/scripts/cuIBM'.format(os.environ['SCRIPTS']))
 import ioCuIBM
 
 
 def parse_command_line():
   """Parses the command-line."""
+  print('[info] parsing the command-line ... '),
   # create parser
   parser = argparse.ArgumentParser(description='Plots the 2D vorticity, '
                                                'pressure and velocity fields',
@@ -28,7 +26,7 @@ def parse_command_line():
                       help='directory of the simulation')
   parser.add_argument('--binary', dest='binary',
                       action='store_true',
-                      help='flag to set if data written in binary format')
+                      help='use flag if data written in binary format')
   # arguments about view
   parser.add_argument('--bottom-left', '-bl', dest='bottom_left', 
                       type=float, nargs='+', default=[float('-inf'), float('-inf')],
@@ -79,82 +77,15 @@ def parse_command_line():
   parser.add_argument('--file', 
                       type=open, action=LoadFromFile,
                       help='path of the file with options to parse')
+  print('done')
   # parse command-line
   return parser.parse_args()
 
 
-def vorticity(u, v):
-  """Computes the vorticity field for a two-dimensional simulation.
-
-  Parameters
-  ----------
-  u, v: ioCuIBM.Field objects
-    u-velocity and v-velocity fields.
-
-  Returns
-  -------
-  vorticity: ioCuIBM.Field object
-    The vorticity field.
-  """
-  print('\tCompute the vorticity field ...')
-  mask_x = numpy.where(numpy.logical_and(u.x > v.x[0], u.x < v.x[-1]))[0]
-  mask_y = numpy.where(numpy.logical_and(v.y > u.y[0], v.y < u.y[-1]))[0]
-  # vorticity nodes at cell vertices intersection
-  xw, yw = 0.5*(v.x[:-1]+v.x[1:]), 0.5*(u.y[:-1]+u.y[1:])
-  # compute vorticity
-  w = ( (v.values[mask_y, 1:] - v.values[mask_y, :-1])
-        / numpy.outer(numpy.ones(yw.size), v.x[1:]-v.x[:-1])
-      - (u.values[1:, mask_x] - u.values[:-1, mask_x])
-        / numpy.outer(u.y[1:]-u.y[:-1], numpy.ones(xw.size)) )
-  return ioPetIBM.Field(x=xw, y=yw, values=w)
 
 
-def plot_contour(field, field_range, image_path, 
-                 view=[float('-inf'), float('-inf'), float('inf'), float('inf')],
-                 size=[8.0, 8.0], dpi=100): 
-  """Plots and saves the field.
 
-  Parameters
-  ----------
-  field: ioPetIBM.Field instance
-    Nodes and values of the field.
-  field_range: list(float)
-    Min, max and number of countours to plot.
-  image_path: str
-    Path of the image to save.
-  view: list(float)
-    Bottom-left and top-right coordinates of the rectangular view to plot;
-    default: the whole domain.
-  size: list(float)
-    Size (width and height) of the figure to save (in inches); default: [8, 8].
-  dpi: int
-    Dots per inch (resolution); default: 100
-  """
-  print('\tPlot the {} contour ...'.format(field.label))
-  fig, ax = pyplot.subplots(figsize=(size[0], size[1]), dpi=dpi)
-  pyplot.xlabel('$x$')
-  pyplot.ylabel('$y$')
-  if field_range:
-    levels = numpy.linspace(field_range[0], field_range[1], field_range[2])
-    colorbar_ticks = numpy.linspace(field_range[0], field_range[1], 5)
-  else:
-    levels = numpy.linspace(field.values.min(), field.values.max(), 101)
-    colorbar_ticks = numpy.linspace(field.values.min(), field.values.max(), 5)
-  X, Y = numpy.meshgrid(field.x, field.y)
-  color_map = {'pressure': cm.jet, 'vorticity': cm.RdBu_r,
-               'u-velocity': cm.RdBu_r, 'v-velocity': cm.RdBu_r}
-  cont = ax.contourf(X, Y, field.values, 
-                     levels=levels, extend='both', 
-                     cmap=color_map[field.label])
-  cont_bar = fig.colorbar(cont, label=field.label, 
-                          orientation='horizontal', format='%.02f', 
-                          ticks=colorbar_ticks)
-  x_start, x_end = max(view[0], field.x.min()), min(view[2], field.x.max())
-  y_start, y_end = max(view[1], field.y.min()), min(view[3], field.y.max())
-  ax.axis([x_start, x_end, y_start, y_end])
-  ax.set_aspect('equal')
-  pyplot.savefig(image_path, dpi=dpi)
-  pyplot.close()
+
 
 
 def main():
@@ -162,54 +93,48 @@ def main():
   for a two-dimensional simulation.
   """
   args = parse_command_line()
-  print('[case directory] {}'.format(args.case_directory))
+  print('[info] case directory: {}'.format(args.case_directory))
 
   time_steps = ioCuIBM.get_time_steps(args.case_directory, args.time_steps)
  
   # create directory where images will be saved
   images_directory = '{}/images'.format(args.case_directory)
-  print('[images directory] {}'.format(images_directory))
+  print('[info] images directory: {}'.format(images_directory))
   if not os.path.isdir(images_directory):
     os.makedirs(images_directory)
 
-  # read the grid nodes
-  coords = ioCuIBM.read_grid(args.case_directory)
-
-  # load default style of matplotlib figures
-  pyplot.style.use('{}/styles/mesnardo.mplstyle'.format(os.environ['SCRIPTS']))
+  coords = ioCuIBM.read_grid(args.case_directory, binary=args.binary)
 
   for time_step in time_steps:
     if args.velocity or args.vorticity:
-      # get velocity fields
       u, v = ioCuIBM.read_velocity(args.case_directory, time_step, coords, 
                                    binary=args.binary)
       if args.velocity:
         # plot u-velocity field
         image_path = '{}/uVelocity{:0>7}.png'.format(images_directory, time_step)
-        plot_contour(u, args.u_range, image_path, 
-                     view=args.bottom_left+args.top_right,
-                     size=args.size, dpi=args.dpi)
+        ioCuIBM.plot_contour(u, args.u_range, image_path, 
+                             view=args.bottom_left+args.top_right,
+                             size=args.size, dpi=args.dpi)
         # plot v-velocity field
         image_path = '{}/vVelocity{:0>7}.png'.format(images_directory, time_step)
-        plot_contour(v, args.v_range, image_path, 
-                     view=args.bottom_left+args.top_right,
-                     size=args.size, dpi=args.dpi)
+        ioCuIBM.plot_contour(v, args.v_range, image_path, 
+                             view=args.bottom_left+args.top_right,
+                             size=args.size, dpi=args.dpi)
       if args.vorticity:
-        # compute vorticity field
-        w = compute_vorticity(u, v)
+        w = ioCuIBM.compute_vorticity(u, v)
         # plot vorticity field
         image_path = '{}/vorticity{:0>7}.png'.format(images_directory, time_step)
-        plot_contour(w, args.vorticity_range, image_path, 
-                     view=args.bottom_left+args.top_right,
-                     size=args.size, dpi=args.dpi)
+        ioCuIBM.plot_contour(w, args.vorticity_range, image_path, 
+                             view=args.bottom_left+args.top_right,
+                             size=args.size, dpi=args.dpi)
     if args.pressure:
-      # get pressure field
-      p = ioCuIBM.read_pressure(args.case_directory, time_step, coords)
+      p = ioCuIBM.read_pressure(args.case_directory, time_step, coords, 
+                                binary=args.binary)
       # plot pressure field
       image_path = '{}/pressure{:0>7}.png'.format(images_directory, time_step)
-      plot_contour(p, args.pressure_range, image_path, 
-                   view=args.bottom_left+args.top_right,
-                   size=args.size, dpi=args.dpi)
+      ioCuIBM.plot_contour(p, args.pressure_range, image_path, 
+                           view=args.bottom_left+args.top_right,
+                           size=args.size, dpi=args.dpi)
 
 
 if __name__ == '__main__':
